ffa107eb988084054e88c77e74f6bd8d0fff3648
twincarlos <twincarlos98@gmail.com>
2024-12-28 17:11:17 -0500

    updated server file

--- server.mjs
+++ server.mjs
@@ -1,40 +1,36 @@
-import next from "next";
 import { Server } from "socket.io";
 import { createServer } from "node:http";
 
-const dev = process.env.NODE_ENV !== "production";
-const hostname = process.env.HOSTNAME;
-const port = 3000;
+const httpServer = createServer();
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
 
-const app = next({ dev, hostname, port })
-const handle = app.getRequestHandler();
+io.on("connection", (socket) => {
+    console.log(`User connected: **${socket.id}**`);
 
-app.prepare().then(() => {
-    const httpServer = createServer(handle);
-    const io = new Server(httpServer);
-
-    io.on("connection", (socket) => {
-        console.log(`User connected: **${socket.id}**`);
-
-        socket.on("join-tournament", ({ id }) => {
-            socket.join(id);
-            console.log(`User joined tournament **${id}**`);
-        });
-        socket.on("update-tournament", ({ tournament }) => {
-            io.emit("update-tournament", tournament);
-            console.log(`Tournament updated: **tournament-${tournament.id}**`);
-        });
-        socket.on("add-tournament", ({ tournament }) => {
-            io.emit("add-tournament", tournament);
-            console.log(`Tournament added: **tournament-${tournament.id}**`);
-        });
-
-        socket.on("disconnect", () => {
-            console.log("User disconnected");
-        });
+    socket.on("join-tournament", ({ id }) => {
+        socket.join(id);
+        console.log(`User joined tournament **${id}**`);
+    });
+    socket.on("update-tournament", ({ tournament }) => {
+        io.emit("update-tournament", tournament);
+        console.log(`Tournament updated: **tournament-${tournament.id}**`);
+    });
+    socket.on("add-tournament", ({ tournament }) => {
+        io.emit("add-tournament", tournament);
+        console.log(`Tournament added: **tournament-${tournament.id}**`);
     });
 
-    httpServer.listen(port, () => {
-        console.log(`Server running on https://${hostname}:${port}`);
+    socket.on("disconnect", () => {
+        console.log("User disconnected");
     });
+});
+
+
+httpServer.listen(port, () => {
+  console.log("WebSocket server running on port 3001");
 });
