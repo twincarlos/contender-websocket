b5388b0ec841fa1ecf602e76f95295ea539cb822
twincarlos <twincarlos98@gmail.com>
2024-12-31 15:07:27 -0500

    passed tournament player data in the tournament player websocket endpoint

--- server.mjs
+++ server.mjs
@@ -10,31 +10,31 @@ const io = new Server(httpServer, {
 });
 
 io.on("connection", (socket) => {
-    console.log(`User connected: **${socket.id}**`);
+  console.log(`User connected: **${socket.id}**`);
 
-    socket.on("join-tournament", (id) => {
-        socket.join(`tournament-${id}`);
-        console.log(`User joined tournament **${id}**`);
-    });
-    socket.on("create-tournament", (tournament) => {
-        io.emit("create-tournament", tournament);
-    });
-    socket.on("update-tournament", (tournament) => {
-        io.emit("update-tournament", tournament);
-        io.to(`tournament-${tournament.id}`).emit("update-single-tournament", tournament);
-    });
-    socket.on("delete-tournament", (id) => {
-      io.emit("delete-tournament", id);
-      io.to(`tournament-${id}`).emit("delete-single-tournament");
-    });
-
-  socket.on("create-tournament-player", (tournamentPlayer) => {
-    io.to(`tournament-${tournamentPlayer.tournamentId}`).emit("create-tournament-player");
+  socket.on("join-tournament", (id) => {
+    socket.join(`tournament-${id}`);
+    console.log(`User joined tournament **${id}**`);
+  });
+  socket.on("create-tournament", (tournament) => {
+    io.emit("create-tournament", tournament);
+  });
+  socket.on("update-tournament", (tournament) => {
+    io.emit("update-tournament", tournament);
+    io.to(`tournament-${tournament.id}`).emit("update-single-tournament", tournament);
+  });
+  socket.on("delete-tournament", (id) => {
+    io.emit("delete-tournament", id);
+    io.to(`tournament-${id}`).emit("delete-single-tournament");
   });
 
-    socket.on("disconnect", () => {
-        console.log("User disconnected");
-    });
+  socket.on("create-tournament-player", (tournamentPlayer) => {
+    io.to(`tournament-${tournamentPlayer.tournamentId}`).emit("create-tournament-player", tournamentPlayer);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
 });
 
 
