d7824ae86356bcfb7fbbb5a5e7fe5b4b6cfbdeed
carlos rodriguez <88858893+twincarlos@users.noreply.github.com>
2024-12-31 14:55:06 -0500

    added create tournament player socket endpoint

--- server.mjs
+++ server.mjs
@@ -16,20 +16,21 @@ io.on("connection", (socket) => {
         socket.join(`tournament-${id}`);
         console.log(`User joined tournament **${id}**`);
     });
-    socket.on("add-tournament", (tournament) => {
-        io.emit("add-tournament", tournament);
-        console.log(`Tournament added: **tournament-${tournament.id}**`);
+    socket.on("create-tournament", (tournament) => {
+        io.emit("create-tournament", tournament);
     });
     socket.on("update-tournament", (tournament) => {
         io.emit("update-tournament", tournament);
         io.to(`tournament-${tournament.id}`).emit("update-single-tournament", tournament);
-        console.log(`Tournament updated: **tournament-${tournament.id}**`);
     });
     socket.on("delete-tournament", (id) => {
       io.emit("delete-tournament", id);
       io.to(`tournament-${id}`).emit("delete-single-tournament");
-      console.log(`Tournament deleted: **tournament-${id}**`);
     });
+
+  socket.on("create-tournament-player", (tournamentPlayer) => {
+    io.to(`tournament-${tournamentPlayer.tournamentId}`).emit("create-tournament-player");
+  });
 
     socket.on("disconnect", () => {
         console.log("User disconnected");
